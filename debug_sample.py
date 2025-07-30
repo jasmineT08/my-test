b3c6e04265eec1ab9b8fe557232657edb4f10fb1
jasmineT08 <wada.10388@gmail.com>
2025-07-30 17:08:56 +0900

    jasmine editted

--- debug_sample.py
+++ debug_sample.py
@@ -1,13 +1,14 @@
-def calculate_sum(a, b):
-    result = a + b
+def calculate_sum(a, b, c):
+    result = a + b + c
     return result
 
 def main():
+    v = 3
     x = 5
     y = 10
     z = calculate_sum(x,y)
 
-    numbers = [1,2,3,4,5]
+    numbers = [1,2,3,4,5,6]
     numbers_squared = [n**2 for n in numbers]
 
     print("xとyの合計:", z)
