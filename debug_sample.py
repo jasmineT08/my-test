77bbf1bf3acecade72106f80b9c2947f92ab8871
Tomomi_Macstudio <wada.10388@gmail.com>
2025-07-30 16:52:33 +0900

    Pythonの使い方

---
+++ debug_sample.py
@@ -0,0 +1,18 @@
+def calculate_sum(a, b):
+    result = a + b
+    return result
+
+def main():
+    x = 5
+    y = 10
+    z = calculate_sum(x,y)
+
+    numbers = [1,2,3,4,5]
+    numbers_squared = [n**2 for n in numbers]
+
+    print("xとyの合計:", z)
+    print("二乗した数列:", numbers_squared)
+
+if __name__ == '__main__':
+
+    main()
